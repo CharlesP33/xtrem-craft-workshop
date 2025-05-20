7ce3a61e5bd994fbacb5de867e849a1a72fc1952
Charles PINET <charles.pinet@etu.u-bordeaux.fr>
2025-05-20 17:11:08 +0200

    Fin étape 9

---
+++ python/src/money.py
@@ -0,0 +1,4 @@
+class Money:
+    def __init__(self, amount: float, currency: str):
+        self.amount = amount
+        self.currency = currency
---
+++ python/src/portfolio.py
@@ -0,0 +1,26 @@
+from python.src.bank import Bank
+from python.src.currency import Currency
+from python.src.money import Money
+
+class Portfolio:
+    
+    def __init__(self):
+        self._amounts = {}
+    
+    def add(self, amount: float, currency: Currency) -> None:
+        if currency in self._amounts:
+            self._amounts[currency] += amount
+        else:
+            self._amounts[currency] = amount
+            
+    def new_add(self, money: Money) -> None:
+        if money.currency in self._amounts:
+            self._amounts[money.currency] += money.amount
+        else:
+            self._amounts[money.currency] = money.amount
+
+    def evaluate(self, bank: Bank, currency: Currency) -> float:
+        total = 0
+        for c in self._amounts.keys():
+            total += bank.convert(self._amounts[c], c, currency)
+        return total
